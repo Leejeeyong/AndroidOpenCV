e330876475606ebdf58800f43c694531142a19d2
Leejeeyong <fghj0720@gmail.com>
2017-02-20 17:26:16 +0900

    second

---
+++ facedectcamera_kotlin/app/src/androidTest/java/com/example/leejy/opencv001/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package com.example.leejy.opencv001
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
